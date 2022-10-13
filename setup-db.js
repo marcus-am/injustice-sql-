94264acf1682904d1a25b3e6fa661dd403f0420a
Dan Keefe <dan@getfutureproof.co.uk>
2022-10-13 12:10:49 +0100

    Update setup-db.js

--- setup-db.js
+++ setup-db.js
@@ -1,8 +1,8 @@
-var fs = require('fs');
+const fs = require('fs');
 require("dotenv").config(); // Load environment config
 
 // Load in the SQL statements
-var sql = fs.readFileSync('setup.sql').toString();
+const sql = fs.readFileSync('setup.sql').toString();
 
 // Get a link to the database
 const db = require("./db");
