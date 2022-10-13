858bf35ef03f07e8f7a7d366a3d8aa8f4cf3013b
Dan Keefe <dan@getfutureproof.co.uk>
2022-10-13 12:01:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config(); // Load environment variables
 
 const api = require("./api");
-const db = require("./db");
 
 api.listen(process.env.PORT, () => {
     console.log(`API listening on port ${process.env.PORT}...`)
